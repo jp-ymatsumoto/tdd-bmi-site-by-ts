fc7a98e0bb86f979078c818a20b5f7597175b351
jp-ymatsumoto <y.matsumoto@iwate-designer.com>
2024-05-11 14:08:51 +0900

    Add: テスト駆動開発の練習問題の基本ファイル
    Add: package.json
    Add: tsconfig.json
    Add: 初期ファイル
    Add: テストファイルの除外
    Add: BMIのフォーム
    Add: ソースファイルとテストファイル
    Add: name属性の追加
    Add: Jest
    Add: jest.config.js
    Add: 空のテストコード
    Add: scriptsにtestの追加
    Add: scriptsにbuildの追加
    Add: プロジェクトの説明文

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+// TODO: テスト済みの関数をインポートしてください
+import {} from "./bmi";
+
+// **************************************************
+// 要素一覧
+// **************************************************
+
+const form = document.getElementById("form") as HTMLFormElement;
+const result = document.getElementById("result") as HTMLDivElement;
+const height = document.getElementById("height") as HTMLInputElement;
+const weight = document.getElementById("weight") as HTMLInputElement;
+
+// **************************************************
+// 処理
+// **************************************************
+
+form.addEventListener("submit", (event: SubmitEvent) => {
+  // TODO: BMI関連の処理を実装してください
+});
